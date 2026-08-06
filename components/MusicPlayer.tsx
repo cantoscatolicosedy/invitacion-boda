cefa993b8ab2cb6994ea1d7513b32bfcbe3554ad
Eduardo <cantos.catolicos.edy@gmail.com>
2026-08-06 13:56:08 -0600

    musica2

--- components/MusicPlayer.tsx
+++ components/MusicPlayer.tsx
@@ -1,19 +1,66 @@
 "use client";
 
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function MusicPlayer() {
   const audioRef = useRef<HTMLAudioElement>(null);
+
   const [playing, setPlaying] = useState(false);
+
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.volume = 0;
+    }
+  }, []);
+
+  const fadeIn = () => {
+    if (!audioRef.current) return;
+
+    let volume = 0;
+
+    audioRef.current.volume = 0;
+    audioRef.current.play();
+
+    const interval = setInterval(() => {
+      volume += 0.05;
+
+      if (!audioRef.current) return;
+
+      audioRef.current.volume = Math.min(volume, 0.35);
+
+      if (volume >= 0.35) {
+        clearInterval(interval);
+      }
+    }, 150);
+  };
+
+  const fadeOut = () => {
+    if (!audioRef.current) return;
+
+    let volume = audioRef.current.volume;
+
+    const interval = setInterval(() => {
+      volume -= 0.05;
+
+      if (!audioRef.current) return;
+
+      audioRef.current.volume = Math.max(volume, 0);
+
+      if (volume <= 0) {
+        audioRef.current.pause();
+        clearInterval(interval);
+      }
+    }, 120);
+  };
 
   const toggleMusic = () => {
     if (!audioRef.current) return;
 
     if (playing) {
-      audioRef.current.pause();
+      fadeOut();
       setPlaying(false);
     } else {
-      audioRef.current.play();
+      fadeIn();
       setPlaying(true);
     }
   };
@@ -22,8 +69,8 @@ export default function MusicPlayer() {
     <>
       <audio
         ref={audioRef}
+        src="/music/perfect.mp3"
         loop
-        src="/music/musica.mp3"
       />
 
       <button
@@ -32,22 +79,33 @@ export default function MusicPlayer() {
           fixed
           bottom-6
           right-6
-          z-[999]
-          h-16
+          z-[9999]
           w-16
+          h-16
           rounded-full
+          border-2
+          border-yellow-300/60
           bg-white/90
+          backdrop-blur-xl
           shadow-2xl
-          backdrop-blur-lg
           flex
           items-center
           justify-center
-          text-2xl
-          hover:scale-110
           transition
+          hover:scale-110
         "
       >
-        {playing ? "⏸" : "🎵"}
+        <div
+          className={`
+            text-3xl
+            ${playing ? "animate-spin" : ""}
+          `}
+          style={{
+            animationDuration: "5s",
+          }}
+        >
+          💿
+        </div>
       </button>
     </>
   );
